7a785761ca36eb4f4e3294cbd73ecbcfd4674a43
Almoe099 <almoe099@gmail.com>
2023-11-10 14:52:02 -0500

    def uniq completed

--- practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd_project.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd_project.rb
@@ -1,3 +1,14 @@
-def uniq(arr)
-    arr * 2
+class Array
+
+    def uniq
+        return self if self.length == 0
+        hash = Hash.new(0)
+        self.each {|el| hash[el] += 1}
+        hash.keys
+    end
+
+    def two_sum
+
+    end
+
 end
